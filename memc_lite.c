16d86eae49ec461ca942fc18a08e878b88ef0f9f
Mikko <mikko@kuut.io>
2013-10-08 18:28:53 +0800

    Update comment and size check

--- memc_lite.c
+++ memc_lite.c
@@ -418,9 +418,13 @@ char *s_uncompress_value (const char *value, size_t *value_len TSRMLS_DC)
 
 	original_size = ntohl (original_size);
 
-	if (original_size < *value_len) {
+	/* Let's make sure that the returned value length is not disproportional.
+	   If original_size is half of the compressed size then something has gone wrong
+	*/
+	if (original_size < (*value_len * 2)) {
 		return NULL;
 	}
+
 	/* Allocate decompression buffer */
 	buffer = emalloc (original_size);
 
